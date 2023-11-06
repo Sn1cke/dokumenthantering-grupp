6ce2c71bf8f9a4126de8aab1c8aac3a61fa8b3e9
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-11-06 15:25:48 +0100

    added nextauth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { EmailIcon, PasswordIcon } from "@/components/icons/inputIcons";
-import Link from "next/link";
+import { signIn } from "next-auth/react";
 import { useState } from "react";
 
 export default function Login() {
@@ -20,56 +19,9 @@ export default function Login() {
       </h2>
 
       <div className="form-control mt-10 mx-auto sm:w-full sm:max-w-sm">
-        <form onSubmit={handleSubmit}>
-          <p className="font-medium text-neutral">Login details</p>
-          <div className="flex items-center text-gray-500 mt-4">
-            <EmailIcon />
-            <input
-              id="email"
-              type="text"
-              placeholder="Email"
-              className={`input input-bordered ${
-                incorrectDetails && `input-error`
-              } w-full pl-10`}
-              onChange={e => {
-                setEmail(e.target.value);
-              }}
-              value={email}
-              required
-            />
-          </div>
-          <div className="mt-2 relative flex items-center text-gray-500">
-            <PasswordIcon />
-            <input
-              id="password"
-              type="password"
-              placeholder="Password"
-              className={`input input-bordered ${
-                incorrectDetails && `input-error`
-              } w-full pl-9`}
-              onChange={e => setPassword(e.target.value)}
-              value={password}
-              required
-            />
-          </div>
-          {/* Preppat för fel inlogg när vi väl gör fetch */}
-          {incorrectDetails && (
-            <label className="label">
-              <span className="label-text-alt text-red-700 font-semibold">
-                Your email or password is incorrect
-              </span>
-            </label>
-          )}
-          <div className="mt-4 text-sm">
-            <span>New member? </span>
-            <Link href="/signup" className="link link-secondary">
-              Join here
-            </Link>
-          </div>
-          <button className="btn btn-active btn-secondary mt-10 w-full">
-            Sign in
-          </button>
-        </form>
+        <button onClick={() => signIn("google", { callbackUrl: "/documents" })} className="btn btn-active btn-secondary mt-10 w-full">
+          Sign in with Google
+        </button>
       </div>
     </div>
   );
